d837ad2a17f0fa52e439a363112a09d355f50a59
Kyle Geiste <kylegeiste@gmail.com>
2013-10-17 19:19:25 -0400

    staging for commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GamesOnRails::Application.config.secret_key_base = '23fb0cc622444b29cfd2be6609af128a899601662848c3186346df2ee8318cc31068d44e527b8dc02ea0adebc5e25ba6cf31a323074e6e8b5b4c2c2145403daa'
