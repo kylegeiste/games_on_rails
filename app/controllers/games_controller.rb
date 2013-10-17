7825033c686dfdaa164c80bb3d873c78b6014386
Kyle Geiste <kylegeiste@gmail.com>
2013-10-17 19:53:56 -0400

    made some changes

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,5 @@
+#class is called GamesController, inherits from ApplicationController
+class GamesController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 GamesOnRails::Application.routes.draw do
+
+#look at the games controller for index
+  root 'games#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
